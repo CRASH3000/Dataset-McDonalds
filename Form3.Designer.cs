81dc9325cfe30785b009bde0e78b536f3024b26c
Alexander Leus <146023882+LeusAlex@users.noreply.github.com>
2024-06-03 18:07:13 +0300

    Add files via upload

---
+++ Form3.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace McDonalds
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Zapusk = new System.Windows.Forms.Button();
+            this.MaxAtextBox2 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.MinAtextBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Category = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.NM = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Komp = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Kaloriy = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Size = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Zapusk
+            // 
+            this.Zapusk.Location = new System.Drawing.Point(433, 7);
+            this.Zapusk.Name = "Zapusk";
+            this.Zapusk.Size = new System.Drawing.Size(206, 76);
+            this.Zapusk.TabIndex = 0;
+            this.Zapusk.Text = "Обновить таблицу";
+            this.Zapusk.UseVisualStyleBackColor = true;
+            this.Zapusk.Click += new System.EventHandler(this.Zapusk_Click);
+            // 
+            // MaxAtextBox2
+            // 
+            this.MaxAtextBox2.Location = new System.Drawing.Point(45, 63);
+            this.MaxAtextBox2.Name = "MaxAtextBox2";
+            this.MaxAtextBox2.Size = new System.Drawing.Size(131, 20);
+            this.MaxAtextBox2.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(223, 61);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(128, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(223, 39);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(128, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // MinAtextBox1
+            // 
+            this.MinAtextBox1.Location = new System.Drawing.Point(45, 39);
+            this.MinAtextBox1.Name = "MinAtextBox1";
+            this.MinAtextBox1.Size = new System.Drawing.Size(131, 20);
+            this.MinAtextBox1.TabIndex = 1;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(45, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(131, 21);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label1.Location = new System.Drawing.Point(223, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(128, 21);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Каллории:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Min:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 66);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(30, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Max:";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(190, 42);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(27, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Min:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(187, 66);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(30, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Max:";
+            this.label5.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Category,
+            this.NM,
+            this.Komp,
+            this.Kaloriy,
+            this.Size});
+            this.dataGridView1.Location = new System.Drawing.Point(15, 123);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(624, 254);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // Category
+            // 
+            this.Category.HeaderText = "Категория";
+            this.Category.Name = "Category";
+            this.Category.ReadOnly = true;
+            // 
+            // NM
+            // 
+            this.NM.HeaderText = "Блюдо";
+            this.NM.Name = "NM";
+            this.NM.ReadOnly = true;
+            this.NM.Width = 200;
+            // 
+            // Komp
+            // 
+            this.Komp.HeaderText = "Выбранный компонент";
+            this.Komp.Name = "Komp";
+            this.Komp.ReadOnly = true;
+            // 
+            // Kaloriy
+            // 
+            this.Kaloriy.HeaderText = "Калорийность";
+            this.Kaloriy.Name = "Kaloriy";
+            this.Kaloriy.ReadOnly = true;
+            // 
+            // Size
+            // 
+            this.Size.HeaderText = "Размер порции";
+            this.Size.Name = "Size";
+            this.Size.ReadOnly = true;
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.MinAtextBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.MaxAtextBox2);
+            this.Controls.Add(this.Zapusk);
+            this.Name = "Form3";
+            this.Text = "Form3";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Zapusk;
+        private System.Windows.Forms.TextBox MaxAtextBox2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox MinAtextBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Category;
+        private System.Windows.Forms.DataGridViewTextBoxColumn NM;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Komp;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Kaloriy;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Size;
+    }
+}
---
+++ Form3.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace McDonalds
+{
+    public partial class Form3 : Form
+    {
+
+        static string[] SplitByCommaWithoutQuotes(string input)   //Пропускаем выражения в кавычках при разбиении на пункты
+        {
+            List<string> parts = new List<string>();
+            bool insideQuotes = false;
+            int startIndex = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == '"')
+                {
+                    insideQuotes = !insideQuotes;
+                }
+                else if (input[i] == ',' && !insideQuotes)
+                {
+                    parts.Add(input.Substring(startIndex, i - startIndex));
+                    startIndex = i + 1;
+                }
+            }
+
+            parts.Add(input.Substring(startIndex));
+
+            return parts.ToArray();
+        }
+
+
+
+        public Form3()
+        {
+            List<string[]> words = new List<string[]>();
+            foreach (string line in File.ReadAllLines("Resources\\dataset - menu McDonalds.csv").Skip(1))
+            {
+                words.Add(SplitByCommaWithoutQuotes(line));     //Загружаем файл в коллекцию массивов строк 
+            }
+            SharedData.Data = words;
+            InitializeComponent();
+            comboBox1.Items.Add("Холестерин");   //10     добавляем значения в раскрывающийся список 
+            comboBox1.Items.Add("Натрий");  //12
+            comboBox1.Items.Add("Углеводы");  //14
+            comboBox1.Items.Add("Сахар");   //18
+            comboBox1.Items.Add("Белок"); //19
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }  //лишнее
+
+        private void Zapusk_Click(object sender, EventArgs e)
+        {
+            int i = 0;
+            string change = comboBox1.SelectedItem.ToString();    // считываем значения из раскрывающегося списка и выясняем номер столбца в data
+            if (change == "Холестерин")
+                i = 10;
+            if (change == "Натрий")
+                i = 12;
+            if (change == "Углеводы")
+                i = 14;
+            if (change == "Сахар")
+                i = 18;
+            if (change == "Белок")
+                i = 19;
+            double elemMin = Double.Parse(MinAtextBox1.Text);          //считываем значения из текстовых ячеек
+            double elemMax = Double.Parse(MaxAtextBox2.Text);
+            double kalorMin = Double.Parse(textBox1.Text);
+            double kalorMax = Double.Parse(textBox2.Text);
+
+            //MessageBox.Show($"{i}, {elemMin}, {elemMax}, {kalorMin}, {kalorMax}");  проверить ввод.
+
+            foreach (string[] word in SharedData.Data)                                       // добавляем в таблицу строки, соотвествующеие критерию 
+                if ((Double.Parse(word[i]) < elemMax & Double.Parse(word[i]) > elemMin)
+                   & (Double.Parse(word[3]) < kalorMax & Double.Parse(word[3]) > kalorMin))
+                    dataGridView1.Rows.Add(word[0], word[1], word[i], word[3], word[2]);
+        }
+    }
+    public static class SharedData    //Открываем общий доступ к файлу Data 
+    {
+        public static List<string[]> Data { get; set; }
+    }
+
+
+}
